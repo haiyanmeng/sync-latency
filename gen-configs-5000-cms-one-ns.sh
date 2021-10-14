f94d9eaf6e4af907acda37f9b16740089c6c7c33
Haiyan Meng <haiyanmeng@google.com>
2021-10-14 11:21:41 -0700

    Add configs-5000-cms-one-ns

---
+++ gen-configs-5000-cms-one-ns.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+  cat > configs-5000-cms-one-ns/ns-1.yaml <<EOF
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: my-ns-1
+EOF
+
+  for cm in {1..5000}
+  do
+  cat > configs-5000-cms-one-ns/cm-${cm}.yaml <<EOF
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: cm-${cm}
+  namespace: my-ns-1
+data:
+  foo: bar
+  bar: my-ns-1
+EOF
+  done
+
+
