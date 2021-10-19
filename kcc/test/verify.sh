4279233cda8b25cc9eff8c5008805698659b4042
Haiyan Meng <haiyanmeng@google.com>
2021-10-19 06:37:27 -0700

    Add kcc/pubsubs

---
+++ kcc/test/verify.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+for name in pubsubtopic.pubsub.cnrm.cloud.google.com/test-cs pubsubsubscription.pubsub.cnrm.cloud.google.com/test-cs-read iampolicymember.iam.cnrm.cloud.google.com/policy-member-binding iamserviceaccount.iam.cnrm.cloud.google.com/pubsub-app
+do
+kubectl wait --for=condition=READY $name -n foo
+done
