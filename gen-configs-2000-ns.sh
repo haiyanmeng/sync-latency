b4eafca8f16830fdc4b95c413bdb0009abbf85d0
Haiyan Meng <haiyanmeng@google.com>
2021-10-21 20:09:00 -0700

    Add configs-2000-ns

---
+++ gen-configs-2000-ns.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+for ns in {1..2000}
+do
+  cat > configs-2000-ns/ns-${ns}.yaml <<EOF
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: my-ns-$ns
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: cm-1
+  namespace: my-ns-$ns
+data:
+  foo: bar
+  bar: my-ns-$ns
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: cm-2
+  namespace: my-ns-$ns
+data:
+  foo: bar
+  bar: my-ns-$ns
+EOF
+done
